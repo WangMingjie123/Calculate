18985729359b2c57b46f4c64cb222436aaf9aec2
WangMingjie123 <15620091261@163.com>
2015-05-28 19:46:54 +0800

    hello

---
+++ src/com/calculate/Calculate.java
@@ -0,0 +1,31 @@
+package com.calculate;
+
+public class Calculate {
+	public int add(int a,int b)
+	{
+		return (a+b);
+	};
+	public int subtract(int a,int b)
+	{
+		return (a-b);
+	};
+	public int multiply(int a,int b)
+	{
+		return (a*b);
+	};
+	public int divide(int a,int b)
+	{
+		if(b==0)
+		{
+			System.out.print("分母不能为0");
+			return 0;
+		}
+		else
+		{
+			
+			return (a/b);
+		}
+		
+	};
+	
+}
---
+++ src/com/calculate/test.java
@@ -0,0 +1,12 @@
+package com.calculate;
+
+public class test {
+	public static void main(String[] args)
+	{
+		Calculate test=new Calculate();
+		System.out.println("加法运算："+test.add(12, 13));
+		System.out.println("减法运算："+test.subtract(18, 2));
+		System.out.println("乘法运算："+test.multiply(2, 6));
+		System.out.println("除法运算："+test.divide(4, 4));
+	}
+}
